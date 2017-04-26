4d885f92a17f51d6ee761cdc551ddcd114d98a9d
David Sisson <davidsissonemail@gmail.com>
2017-04-25 21:14:46 -0700

    Added sqlite db - potential inventory

---
+++ cleanmp3/db.py
@@ -0,0 +1,32 @@
+import sqlite3
+
+class datao(object):
+
+	def __init__(self):
+		self.connection = ''
+		self.new = 1
+		self.database = 'dat.db'
+		self.dbconnection()
+		self.firstruncheck()
+
+
+	def dbconnection(self):
+		self.connection = sqlite3.connect(self.database)
+		return
+
+	def firstruncheck(self):
+		sql = "SELECT count(*) from inventory1;"
+		print(self.connection)
+		try:
+			conn = self.connection
+			c = conn.cursor()
+			c.execute(sql)
+			returndata = c.fetchall()
+			#print('1):', returndata)
+			self.new = 0
+		except:
+			self.new = 1
+			#print("non")
+
+	def close(self):
+		self.connection.close()
--- cleanmp3/main.py
+++ cleanmp3/main.py
@@ -1,4 +1,6 @@
 import os
+import sqlite3
+from db import datao
 
 from func import media
 from func import traverse
@@ -7,7 +9,16 @@ from func import traverse
 test = '/mnt/h/nas/Music/a/Adele/singles/cover.jpg'
 test = '/mnt/h/nas/Music/a/Adele/'
 
+
 myitem = media()
 print(myitem.gettype(test))
 
 traverse(test)
+
+
+dbcon = datao()
+print(dbcon.new)
+
+#conn.commit()
+dbcon.close()
+
